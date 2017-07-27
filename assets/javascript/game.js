dddd8c2f2dbd2e22214e201b684a520f528dd1df
Andrea Pinillos <andreapinillos@Andreas-MacBook-Pro.local>
2017-07-26 22:43:34 -0400

    javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,39 @@
-window.onload = function () {
-	// Creates an array that lists out all of the options (alphabet).
-  var alphabetLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
- 	// Creating variables to hold the number of wins, losses, guess left, and guesses. They start at 0.
     var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
-    // guessesSoFar is an array that will hold all the user's guesses in each round
-	var guessesSoFar = [];
-	// userGuess is what the user picks by pressing a key
-	var userGuess = null;
-
-	// Have computer pick a letter and store it in letterToBeGuessed
-    var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
-    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
-
-    // This function is run whenever the user presses a key.
+    var guessesSoFar = []; // array to push user choices to
+    var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //list of letters to choose from
     document.onkeyup = function(event) {
-    // Determines which key was pressed.
-    var userGuess = event.key;
-
-    // When user presses a key, it records it and saves to userGuess
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-      
-
-	// Add the user's guess to guessesSoFar array but only if it wasn't already previously picked by the user
-	// make sure that the character user picks is within the alphabet, and not any non-usable character
-	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
-		guessesSoFar[guessesSoFar.length]=userGuess;
-		// if it is a new letter then decrease remaining guesses by 1
-		guessesLeft--;
-
-	// if letterToBeGuessed is same as userGuess then record it as a win
-	// and then reset guessesLeft to 9, and empty the guessesSoFar array
-	// also have the computer make a new random pick
-	if (letterToBeGuessed == userGuess) {
-		wins++;
-		console.log("You won!");
-		guessesLeft = 9;
-		guessesSoFar = [];
-		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
-		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
-	}
-
-	// if guessesLeft gets to 0 then record it as a loss
-	// and then reset guessesLeft to 9, and empty the guessesSoFar array
-	// also have the computer make a new random pick
-	if (guessesLeft == 0) {
-		losses++;
-		console.log("You lost!");
-		guessesLeft = 9;
-		guessesSoFar = [];
-		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
-		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
-	}
-
-	// Displaying progress to HTML
-	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
-	// place html into the game ID
-	document.querySelector("#game").innerHTML = html;
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); //taking in user guess
+        var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)]; //computer selects random letter
+        guessesSoFar.push(userGuess); //pushing user guess to guesses so far
+        if (userGuess == computerGuess) {
+            wins++;
+            alert('Way to go! You guessed it correctly!');
+            guessesLeft = 9; //reseting the guesses back to 9 so that the user can play again
+            guessesSoFar.length = 0; //this removes everything from the guesses so far array, so that the guesses from the previous round don't show
+        }
+        else if (guessesLeft == 0){
+            losses++;
+            alert('You did not guess the correct letter, try again!');
+            guessesLeft = 9;
+            guessesSoFar.length = 0;
+        }
+        else if (userGuess !== computerGuess){
+            guessesLeft--; //decrementing the guesses left
+        }  
+        // Taking the tallies and displaying them in HTML    
+        var html = "<h1>The Psychic Game</h1><br>" + 
+        "<p>Guess what letter I'm thinking of!</p><br>" +
+        "<p>Total Wins: " + 
+        wins + "</p>" +
+        "<p>Total Losses: " + 
+        losses + "</p>" +
+        "<p>Guesses Left: " + 
+        guessesLeft + "</p>" +
+        "<p>Your Guesses so far: " +
+        guessesSoFar + "</p>"
+        ;
+        // Placing the html into the game ID
+        document.querySelector('#game').innerHTML = html;
+    }
